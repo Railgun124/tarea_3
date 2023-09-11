bb0a39686e3c5e6d58e41650db343b426221d04e
Railgun124 <majorityaxe111.jg@gmail.com>
2023-09-10 21:20:03 -0700

    pagina completa de presentacion

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tarea_3
+Integrantes del equipo 
+Sandra Machuca
+Victor Vazquez
+Juan Graxiola
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plataformas de desarrollo colaborativo y control de versiones</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f7f7f7;
+        }
+
+        nav {
+            background-color: #0078d4;
+            color: #fff;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        h1 {
+            margin: 0;
+            padding: 0;
+            font-size: 24px;
+        }
+
+        main {
+            max-width: auto;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        #volver {
+            position: fixed;
+            bottom: 20px;
+            left: 20px;
+            background-color: #0078d4;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            z-index: 999;
+            opacity: 0.7;
+            transition: opacity 0.3s ease-in-out;
+        }
+
+        #volver:hover {
+            opacity: 1;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <h1>Plataformas de desarrollo colaborativo y control de versiones</h1>
+    </nav>
+    <main>
+    <div style="width: 100%;"><div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;"><iframe title="Presentación de plataformas de desarrollo colaborativo" frameborder="0" width="1200px" height="675px" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/64fe7e6aa4860700186f3f0d" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> </div> </div>
+    </main>
+    <a href="../index.php"><button id="volver">Volver</button></a>
+</body>
+</html>
